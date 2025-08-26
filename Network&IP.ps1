a88005bab311ee32016cb3c443af313acdef28f8
ArunPratapSingh <180103642+ArunPratapSinghh@users.noreply.github.com>
2025-08-26 09:51:03 +0530

    Create Network&IP.ps1

---
+++ Network&IP.ps1
@@ -0,0 +1,7 @@
+Write-host "Public IP Address"
+
+New-AzPublicIpAddress -Name "PIP-Demo" -ResourceGroupName "RG-DemoApp-EastUS" -Location "EastUS" -AllocationMethod Static -Sku Standard
+
+Write-host "Network Interface"
+
+New-AzNetworkInterface -Name "NIC-DemoVM" -ResourceGroupName "RG-DemoApp-EastUS" -Location "EastUS" -SubnetId $vnet.Subnets[0].Id -PublicIpAddressId $pip.Id -NetworkSecurityGroupId $nsg.Id
